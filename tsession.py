6a3dc6a5e0768cd51fb6d552bc409ab0b37e08b4
mccoolsa <126930397+mccoolsa@users.noreply.github.com>
2025-05-08 00:35:13 +0100

    Update tsession.py
    twitter login script

--- tsession.py
+++ tsession.py
@@ -1,16 +1,19 @@
+# script to pull twitter sessions login
+# necessary for pulling information on privated tweets
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=False) #launch Chrome externally
     context = browser.new_context()
     page = context.new_page()
     
-    # Open login page
+    #open login page
     page.goto("https://twitter.com/login")
     print("🔐 Log in manually in the opened browser window.")
-    input("✅ After logging in and closing the browser, press Enter here...")
+    input("✅ After logging in and closing the browser, press Enter here...") #hit enter on console after logging in
+    
 
-    # Save session
-    context.storage_state(path="twitter_session.json")
+    #save session
+    context.storage_state(path="twitter_session.json") #stores login as json file - needed for future
     print("💾 Session saved to twitter_session.json")
     browser.close()
